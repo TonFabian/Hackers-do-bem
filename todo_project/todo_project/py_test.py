169da7601165824bf8a0d177b80ff110e381953b
TonFabian <wertonf4bian@gmail.com>
2024-09-13 10:14:21 -0300

    py_test atualizado

--- todo_project/todo_project/py_test.py
+++ todo_project/todo_project/py_test.py
@@ -1,3 +1,8 @@
-# test_sample.py
-def test_example():
-    assert 1 + 1 == 2
+import unittest
+
+class TestMathOperations(unittest.TestCase):
+    def test_addition(self):
+        self.assertEqual(1 + 1, 2)
+
+if __name__ == '__main__':
+    unittest.main()
