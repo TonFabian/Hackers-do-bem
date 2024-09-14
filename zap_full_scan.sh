59c28bf7bacd450455d4b98617299cc8db3c608f
TonFabian <wertonf4bian@gmail.com>
2024-09-14 13:26:18 -0300

    Estudo de Caso 2.0

--- zap_full_scan.sh
+++ zap_full_scan.sh
@@ -1,22 +1,23 @@
-#!/bin/bash
-
-# Substituição das variáveis no arquivo de configuração
-sed -i "s/PASSWORD/$PASSWORD/g" /zap/zap_config.conf
-sed -i "s/USERNAME/$USERNAME/g" /zap/zap_config.conf
-sed -i "s/ZAP_REPORT/$ZAP_REPORT/g" /zap/zap_config.conf
-sed -i "s/CI_PROJECT_DIR/$CI_PROJECT_DIR/g" /zap/zap_config.conf
-sed -i "s/ZAP_ALERT_REPORT/$ZAP_ALERT_REPORT/g" /zap/zap_config.conf
-
-# Executar o ZAP no modo de análise completa
-/zap/zap.sh -cmd -quickurl http://my-app:5000 -quickout /zap/${ZAP_REPORT}.html
-
-# Verificação dos resultados
-returnCode=0
-if grep -q "Instances" /zap/${ZAP_ALERT_REPORT}.md; then
-  head -n 20 /zap/${ZAP_ALERT_REPORT}.md
-  echo "DAST RESULT: There are some vulnerabilities that ZAP has found (those visible here may not be the only ones). See the detailed report for more information."
-  returnCode=1
-fi
-
-exit $returnCode
-
+zap-full-scan:
+  stage: scan
+  image:
+    name: zaproxy/zap-stable:2.15.0
+    entrypoint: [""]
+  variables:
+    CI_PROJECT_DIR: /zap/wrk
+    ZAP_ALERT_REPORT: quick_scan_alert_report
+    ZAP_REPORT: quick_scan_report
+  script:
+    - echo "Copying files from /zap to /zap/wrk..."
+    - mkdir -p /zap/wrk  # Garante que o diretório /zap/wrk exista
+    - cp -r /zap/* /zap/wrk/
+    - bash /zap/zap_full_scan.sh
+  only:
+    refs:
+      - branches
+  artifacts:
+    when: always
+    expire_in: 1 week
+    paths:
+      - wrk/quick_scan_report.html
+      - wrk/quick_scan_alert_report.md
